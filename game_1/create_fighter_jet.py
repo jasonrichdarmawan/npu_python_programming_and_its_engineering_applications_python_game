821c850099d6e24b90c21817be7ea80c6481dcbe
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2024-11-21 18:23:46 +0800

    chore: make the spawn more predictable

--- game_1/create_fighter_jet.py
+++ game_1/create_fighter_jet.py
@@ -6,6 +6,10 @@ from game_window import GAME_WINDOW
 
 def create_fighter_jet(type: str, append_bullet_in_flight: Callable[[Bullet], None]) -> FighterJet:
     x = random.randint(0, GAME_WINDOW.get_width())
-    y = random.randint(0, GAME_WINDOW.get_height())
+    match type:
+        case "enemy":
+            y = random.randint(0, 100)
+        case "fighter_1" | "fighter_2":
+            y = random.randint(GAME_WINDOW.get_height() - 100, GAME_WINDOW.get_height())
     fighter_jet = FighterJet(type, 5, x, y, 1000, append_bullet_in_flight)
     return fighter_jet
